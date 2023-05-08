78c8b8e5cfcd02e960e4ccfa8a6bc00e033996fa
timoxa2t <alex2t172@gmail.com>
2023-05-08 23:55:14 +0300

    endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 
-const phones = require('../public/phones.json')
-// main file to Create API Server
-
-// pattern for CreateServer:
-
+const phones = require('../public/api/phones.json');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
+
 const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send("<p>Hello world!</p>");
@@ -16,6 +18,32 @@ app.get('/', (req, res) => {
 
 app.get('/phones', (req, res) => {
   res.json(phones);
+});
+
+app.get('/phones/:phoneId', (req, res) => {
+  const { phoneId } = req.params
+
+  const filePath = path.join(
+    __dirname,
+    '..',
+    'public',
+    'api',
+    'phones',
+    `${phoneId}.json`,
+  );
+
+  console.log(filePath)
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.sendStatus(404)
+  
+      return
+    }
+
+    res.json(JSON.parse(Buffer.from(data).toString()))
+
+  });
 });
 
 app.listen(PORT, () => {
