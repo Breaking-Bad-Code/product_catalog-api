b0ed08bd27491912ac234f40a7436091656a4117
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-05-08 14:21:30 +0300

    API starter config v2

---
+++ main.js
@@ -0,0 +1,17 @@
+// main file to Create API Server
+
+// pattern for CreateServer:
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("<p>Hello world!</p>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
